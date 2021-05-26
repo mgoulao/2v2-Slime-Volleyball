291af62070db012337af26203edabf610597fe7b
stopshark <stopshark@github.com>
2021-05-26 22:34:49 +0100

    role stuff

--- study/selfplay.py
+++ study/selfplay.py
@@ -4,8 +4,7 @@ sys.path.append('../slimevolleygymrepo')
 
 import slimevolleygym 
 
-BEST_THRESHOLD = 8
-
+BEST_THRESHOLD = 7.5
 class SlimeVolleySelfPlayEnv(slimevolleygym.SlimeVolleyEnv):
   # wrapper over the normal single player env, but loads the best self play model
   def __init__(self, team, renderMode, selfplay):
--- study/train_ppo.py
+++ study/train_ppo.py
@@ -20,7 +20,7 @@ SELFPLAY = True
 if __name__=="__main__":
 
     parser = argparse.ArgumentParser(description='Train PPO.')
-    parser.add_argument('--render', action='store_true', help='Enable environment render', default=False)
+    parser.add_argument('--render', action='store_true', help='Enable environment render', default=True)
     parser.add_argument('--noselfplay', action='store_true', help='Disable selfplay', default=False)
     args = parser.parse_args()
 
